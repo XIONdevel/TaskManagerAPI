652613c17b47836e6f75c5f271e4ee6cc8b19525
XION <igor97645@gmail.com>
2024-08-08 12:37:12 +0300

    init commit

---
+++ src/test/java/org/noix/taskmanagement/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.noix.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
